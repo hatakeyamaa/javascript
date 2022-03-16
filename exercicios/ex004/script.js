713379c9d1c778e33f73e90efa39749d3e6e78cd
hatakeyamaa <97980944+hatakeyamaa@users.noreply.github.com>
2022-03-16 18:46:15 -0300

    ex004

--- exercicios/ex004/script.js
+++ exercicios/ex004/script.js
@@ -1,1 +1,38 @@
+/*
+var n = 4
+var f = n*10
+var p = n
 
+for (n > 0; n <= f; n += p) {
+    console.log(n)
+}
+*/
+
+function gerar() {
+    let num = document.querySelector('input#numtxt')
+    let res = document.querySelector('select#listatxt')
+    
+    if (num.value.length == 0) {
+        window.alert('[ERRO] você precisa digitar um número!') 
+        
+    } else {
+        let n = Number(num.value)
+        c = 1
+
+        for (c > 0; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            res.appendChild(item)
+        }
+
+        /*
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            res.appendChild(item)
+            c++
+        }
+        */
+     
+    }
+}
