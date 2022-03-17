c267cb9b9bbf75d63c162cec2fca6e63acdc77fd
hatakeyamaa <97980944+hatakeyamaa@users.noreply.github.com>
2022-03-17 17:13:24 -0300

    aula

---
+++ exercicios/aula16/funcao01.js
@@ -0,0 +1,15 @@
+
+function parimp(n) {
+    if(n%2 == 0) {
+        return 'Par'
+    } else {
+        return 'Impar'
+    }
+} 
+/*
+let res = parimp()
+console.log(res)
+*/
+console.log(parimp(4))
+
+
