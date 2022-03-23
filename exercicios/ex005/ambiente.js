caf62791f5c9a0568bb5687a926ad2b418f28d12
hatakeyamaa <97980944+hatakeyamaa@users.noreply.github.com>
2022-03-23 13:52:58 -0300

    ex

--- exercicios/ex005/ambiente.js
+++ exercicios/ex005/ambiente.js
@@ -5,7 +5,7 @@ function generateRange(n) {
     return false;
 }
 console.log(generateRange(1));
-*/
+
 var num = [1 <= 100]
 
 let n = Number(num.value)
@@ -19,4 +19,15 @@ if(numero.indexOf(n) != -1) {
     res.appendChild(item)
 } else if (numero.includes(n) > -1) { 
     window.alert(`O número já existe`)
+}
+*/
+
+function maior(nums) {
+    let maiorNumero = nums[1]
+    for(let m = 1; m < nums.length; m++) {
+        if (nums[m] > maiorNumero) {
+            nums[m] = maiorNumero
+        }
+    }
+    return maiorNumero
 }
--- exercicios/ex005/script.js
+++ exercicios/ex005/script.js
@@ -1,19 +1,42 @@
+let numero = document.querySelector('input#numtxt')
+let res = document.querySelector('select#res1')
+let res2 = document.querySelector('div#res2')
+
+
+let n = Number(numero.value)
+let num = [1, 2, 3, 4]
+
 function adc() {
-    let numero = document.querySelector('input#numtxt')
-    let res = document.querySelector('select#res1')
 
-    let n = Number(numero.value)
-    //let num = [1, 2, 3]
 
+        if(numero.value.length == 0 || numero.value == 0) {
+        window.alert('Digite um número entre 1 e 100')
+        }
         if (num.indexOf(n) === -1) {
             num.push(n);
             let item = document.createElement('option')
             res.appendChild(item)
             item.text = `O número ${n} foi adicionado`
-        } else if (num.indexOf(n) > -1) {
+        } else if (num.indexOf(n)) {
             window.alert(`O número já existe`)  
         }
-        
+}   
+
+function finalizar() {
+    soma = 0
+    for(let s = 0; s < num.length; s++) {
+        soma += num[s]
+    }
+
+    if(let maior = ) {
+
+    }
+    res2.innerHTML = `<p>Ao todo temos ${num.length} números cadastrados</p> 
+    <p>O maior valor informado foi</p> 
+    <p>O menor valor informado foi </p> 
+    <p>Somando todos os valores, temos ${soma}</p> 
+    <p>A média dos valores digitados é </p>`
+}
 
 
 
@@ -79,5 +102,5 @@ function adc() {
             window.alert(`Digite um número menor que ${n}`)
         }
     }*/
-}
+
 
