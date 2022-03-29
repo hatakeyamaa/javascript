0ab68b1b8bfd905438be3eda190d2d20a1fc115d
hatakeyamaa <97980944+hatakeyamaa@users.noreply.github.com>
2022-03-29 18:26:47 -0300

    att desafios

---
+++ desafios/d007/script.js
@@ -0,0 +1,6 @@
+let dolar = Number(window.prompt(`Quanto está a cotação do dólar agora?`))
+let res = document.querySelector('div#res')
+
+function converter() {
+    let rs = window.prompt(`Quantos reais você possui?`)
+}
