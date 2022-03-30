12303aab2acbec9c35e579a93426905a10c33d58
hatakeyamaa <97980944+hatakeyamaa@users.noreply.github.com>
2022-03-30 15:42:41 -0300

    desafio

--- desafios/d007/script.js
+++ desafios/d007/script.js
@@ -1,6 +1,11 @@
 let dolar = Number(window.prompt(`Quanto está a cotação do dólar agora?`))
 let res = document.querySelector('div#res')
 
-function converter() {
-    let rs = window.prompt(`Quantos reais você possui?`)
+function botao() {
+    let rs = Number(window.prompt(`Quantos reais você possui?`))
+
+    let cotação = (rs / dolar).toLocaleString('pt-BR')
+
+    res.innerHTML = `<h2>Conversão de Reais para Dólares</h2>`
+    res.innerHTML += ``
 }
